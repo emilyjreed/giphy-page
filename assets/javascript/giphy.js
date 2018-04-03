566d778091af60ef1278595f3d4bd26e36559e98
emilyjreed <emilykjackson@gmail.com>
2018-04-03 16:19:58 -0400

    third commit. fixed image alignment

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -3,27 +3,28 @@ var tvShows = ["The Office", "Archer", "Parks and Recreation", "Bob's Burgers", 
 function displayGiphyImages() {
     var show = $(this).attr("data-show");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        $(this).data("show") + "&api_key=F7N29iBpxvGva62UI6K5nNANg5QteRhH&limit=12";
+        $(this).data("show") + "&api_key=F7N29iBpxvGva62UI6K5nNANg5QteRhH&limit=10";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         .done(function (response) {
-            var showData = $("<div class='show'>");
             $(".show").empty();
             var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
-                var p = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
+                var figcap = $("<figcaption>").text("Rating: " + results[i].rating.toUpperCase());
+                var figure = $("<figure class='show'>");
                 var showImage = $("<img>");
                 showImage.addClass("gif");
+                figure.addClass("float-left");
                 showImage.attr("src", results[i].images.fixed_height_still.url);
                 showImage.attr("data-still", results[i].images.fixed_height_still.url);
                 showImage.attr("data-animate", results[i].images.fixed_height.url);
                 showImage.attr("data-state", "still");
-                showData.append(showImage, p);
-                $("#main-content").append(showData);
+                figure.append(showImage, figcap);
+                $("#main-content").append(figure);
             }
         });
 };
